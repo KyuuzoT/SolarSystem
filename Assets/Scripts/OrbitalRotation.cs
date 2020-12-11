fb4f2d102fe770a2727d9ab52792dd35d0ee7404
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-12-11 23:24:36 +0300

    last edition

--- Assets/Scripts/OrbitalRotation.cs
+++ Assets/Scripts/OrbitalRotation.cs
@@ -16,10 +16,14 @@ public class OrbitalRotation : MonoBehaviour
     private Vector3 offset;
     private float rotationY = 0.0f;
 
+    private Vector3[] coords;
+
     // Start is called before the first frame update
     void Start()
     {
+        coords = new Vector3[1];
         offset = pivotOrbitalRotationPoint.transform.position - transform.position;
+        coords[0] = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -29,5 +33,10 @@ public class OrbitalRotation : MonoBehaviour
         rotationY += (1.0f / rotationSpeed)*multiplier;
         Quaternion rotation = Quaternion.Euler(0, rotationY, 0);
         transform.position = pivotOrbitalRotationPoint.transform.position - ((rotation) * offset);
+
+        if(Mathf.Abs(transform.position.x/coords[0].x) <= 0.1f)
+        {
+            //Debug.Log("RETURNED" + gameObject.name);
+        }
     }
 }
---
+++ Assets/Scripts/OrbiteLineController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbiteLineController : MonoBehaviour
+{
+    private LineRenderer line;
+    private Transform[] points;
+    [SerializeField][Range(0.01f, 1.0f)]
+    private float lineMultiplier = 0.1f;
+
+    private void Awake()
+    {
+        line = gameObject.AddComponent<LineRenderer>();
+        line.widthMultiplier = lineMultiplier;
+    }
+
+    internal void SetUpLine(Transform[] points)
+    {
+        line.positionCount = points.Length;
+        this.points = points;
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            line.SetPosition(i, points[i].position);
+        }
+    }
+}
