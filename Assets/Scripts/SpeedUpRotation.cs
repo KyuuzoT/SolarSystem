082ead9c1ff220832ec9af14f563eb428b0d7265
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-02-18 21:07:54 +0300

    Starting changing project for control task

--- Assets/Scripts/SpeedUpRotation.cs
+++ Assets/Scripts/SpeedUpRotation.cs
@@ -7,17 +7,35 @@ public class SpeedUpRotation : MonoBehaviour
     [SerializeField][Range(1,1000)]
     private float rotationSpeedMultiplier = 1.0f;
 
-    public Rect Label;
-    public Rect Slider;
+    private Rect label = new Rect();
+    private Rect slider;
+
+    public Rect Label
+    {
+        get => label;
+        set
+        {
+            label= value;
+        }
+    }
+
+    public Rect Slider
+    {
+        get => slider;
+        set 
+        {
+            slider = value;
+        }
+    }
+    internal float RotationSpeedMultiplier
+    {
+        get => rotationSpeedMultiplier;
+    }
 
     private void Awake()
     {
         Label = new Rect(Screen.width*0.6f / 2, Screen.height * 0.9f, 250, 20);
         Slider = new Rect(Screen.width*0.9f / 2, Screen.height * 0.915f, 250, 20);
-    }
-    internal float RotationSpeedMultiplier
-    {
-        get => rotationSpeedMultiplier;
     }
 
     private void OnGUI()
